02f9e2eece948961c7c27b0cd32b89fc98267670
AndreAggalut <aaggalut3895@gmail.com>
2019-12-17 10:46:29 -0800

    "added burger builder"

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import '../Burger/Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transformedIngredient = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+           return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        
+        }); // an array for two ingredientss
+    })
+    .reduce((arr,curr) => {
+        return arr.concat(curr);
+    }, []);
+    if(transformedIngredient.length === 0){
+        transformedIngredient= <p>Please start adding ingredients!</p>
+    }
+    console.log(transformedIngredient)
+        return (
+            <div className='Burger'>
+                <BurgerIngredient type ="bread-top"/>
+                {transformedIngredient}
+                <BurgerIngredient type ="bread-bottom"/>
+                
+            </div>
+        )
+    }
+export default burger;
